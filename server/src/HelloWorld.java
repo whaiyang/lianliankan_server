9cae3988d06ab17775c36d62b4457b10e0ee1957
whaiyang <915505607@qq.com>
2019-09-01 17:11:35 +0800

    Initial commit

---
+++ server/src/HelloWorld.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println();
+        ArrayList<String> strings = new ArrayList<>();
+        if (strings == null) {
+
+        }
+
+        for (String string : strings) {
+
+        }
+    }
+}
